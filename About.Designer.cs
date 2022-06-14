352f6449db9f5a58f54ec6c43927fd93a701c042
rex1431ify <r.l@live.se>
2022-06-14 20:32:01 +0200

    source code

---
+++ About.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace notepad
+{
+    partial class About
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label = new System.Windows.Forms.Label();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.butnOk = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(166, 70);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(38, 15);
+            this.label.TabIndex = 0;
+            this.label.Text = "label1";
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(0, 0);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 0;
+            // 
+            // butnOk
+            // 
+            this.butnOk.Location = new System.Drawing.Point(97, 162);
+            this.butnOk.Name = "butnOk";
+            this.butnOk.Size = new System.Drawing.Size(75, 23);
+            this.butnOk.TabIndex = 1;
+            this.butnOk.Text = "&Ok";
+            this.butnOk.UseVisualStyleBackColor = true;
+            this.butnOk.Click += new System.EventHandler(this.butnOk_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Notepad created by Rasmus Liljefors";
+            // 
+            // About
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.butnOk);
+            this.Name = "About";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label;
+        private Button btnOk;
+        private Button butnOk;
+        private Label label1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,118 @@
+namespace notepad
+{
+    public partial class Form1 : Form
+    {
+        public About About { get; private set; }
+        string path;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            path = string.Empty;
+            textBox.Clear();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Text Documents|*.txt", ValidateNames = true, Multiselect = false })
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        
+                        using (StreamReader sr = new StreamReader(ofd.FileName))
+                        {
+                            path = ofd.FileName;
+                            Task<string> text = sr.ReadToEndAsync();
+                            textBox.Text = text.Result;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+        }
+
+        private async void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                using (SaveFileDialog sfd = new SaveFileDialog() { Filter = " Text Documents|*.txt", ValidateNames = true })
+                {
+                    try
+                    {
+                        if (sfd.ShowDialog() == DialogResult.OK)
+                        {
+                            using (StreamWriter sw = new StreamWriter(sfd.FileName))
+                            {
+                                await sw.WriteLineAsync(textBox.Text);
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                try
+                {
+                    using (StreamWriter sw = new StreamWriter(path))
+                    {
+                        await sw.WriteLineAsync(textBox.Text);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private async void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog sfd = new SaveFileDialog() { Filter = " Text Documents|*.txt", ValidateNames = true })
+            {
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        using (StreamWriter sw = new StreamWriter(sfd.FileName))
+                        {
+                            await sw.WriteLineAsync(textBox.Text);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (About = About = new About())
+            {
+                About.ShowDialog();
+            }
+        }
+    }
+}
